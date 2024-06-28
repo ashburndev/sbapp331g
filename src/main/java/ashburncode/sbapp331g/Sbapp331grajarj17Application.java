4f7a41b0b229a91e7b2a99eb01c8de21dbeacc46
ashburndev <ashburndev@gmail.com>
2024-06-28 08:30:42 -0400

    initial commit

---
+++ src/main/java/ashburncode/sbapp331g/Sbapp331grajarj17Application.java
@@ -0,0 +1,13 @@
+package ashburncode.sbapp331g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbapp331grajarj17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbapp331grajarj17Application.class, args);
+	}
+
+}
---
+++ src/test/java/ashburncode/sbapp331g/Sbapp331grajarj17ApplicationTests.java
@@ -0,0 +1,13 @@
+package ashburncode.sbapp331g;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbapp331grajarj17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
